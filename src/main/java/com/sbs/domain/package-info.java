76724602a5b2596e0598f25814801bd6b431d224
gdarshit <gadhia.darshit@oracle.com>
2023-07-01 18:34:47 +0530

    Initial version of sbs generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/sbs/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.sbs.domain;
---
+++ src/test/java/com/sbs/IntegrationTest.java
@@ -0,0 +1,25 @@
+package com.sbs;
+
+import com.sbs.SbsApp;
+import com.sbs.config.AsyncSyncConfiguration;
+import com.sbs.config.EmbeddedRedis;
+import com.sbs.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { SbsApp.class, AsyncSyncConfiguration.class })
+@EmbeddedRedis
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
